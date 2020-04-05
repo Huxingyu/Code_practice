5df51f0bf04d787c7674683278a8f20ee4d75c5c
胡行宇 <elgooguknow@gmail.com>
2020-04-05 23:26:59 +0800

    1

---
+++ test/000.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "huhu.h"
+
+int main(){
+    printf("%d\n",huhu_max(1,2));
+    system("pause");
+    return 0;
+}
---
+++ test/huhu.h
@@ -0,0 +1,8 @@
+int huhu_max(int x,int y){
+    if(x>y){
+        return x;
+    }
+    else{
+        return y;
+    }
+}
