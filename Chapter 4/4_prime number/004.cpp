02913fed4d9c6e43ecfb9736ff173f8b6e7bed96
胡行宇 <elgooguknow@gmail.com>
2020-04-07 20:44:07 +0800

    4

--- Chapter 4/4_prime number/004.cpp
+++ Chapter 4/4_prime number/004.cpp
@@ -7,7 +7,7 @@ bool is_prime[max]={0};
 
 void cal_prime(int n){
     for(int i=2;i<max;i++){
-        if(is_prime[i]==false){
+        if(is_prime[i]==false){             //判断条件
             prime[num++]=i;
             for(int j=i+i;j<max;j+=i){
                 is_prime[j]=true;
--- Chapter 4/5_prime factor/002.cpp
+++ Chapter 4/5_prime factor/002.cpp
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define max 100
+#define max 100000
 
 int num=0,prime[max];
 bool is_prime[max]={0};
 
-struct fac{
-    int fac,num;
-}prime_num[10];
-
-void cal_prime(int n){
+void cal_prime(){
     for(int i=2;i<max;i++){
         if(is_prime[i]==false){
             prime[num++]=i;
@@ -17,19 +13,12 @@ void cal_prime(int n){
                 is_prime[j]=true;
             }
         }
-        if(num>=n){
-            printf("%d\n",num);
-            break;
-        }
     }
 }
 
-void cal_factor_prime(int x){
-    //理解错题意了
-}
-
 int main(){
-    cal_prime(10);
+    cal_prime();
+    printf("%d\n",num);
     for(int i=0;i<num;i++){
         printf("%d ",prime[i]);
     }
