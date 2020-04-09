59bbb36d5705d6f6194e81e46878c854e5c6b8af
胡行宇 <elgooguknow@gmail.com>
2020-04-09 23:39:57 +0800

    2

--- Chapter 4/6_large Integer/006.cpp
+++ Chapter 4/6_large Integer/006.cpp
@@ -4,10 +4,10 @@
 #define max 100
 
 struct bign{
-    int num[max];
+    int big_num[max];
     int len;
     bign(){
-        memset(num,0,sizeof(num));
+        memset(big_num,0,sizeof(big_num));
         len=0;
     }
 };
@@ -16,25 +16,24 @@ bign big_save(char str[]){
     bign x;
     int size=strlen(str);
     for(int i=0;i<size;i++){
-        x.num[i]=str[size-1-i]-'0';
+        x.big_num[i]=str[size-1-i]-'0';
         x.len++;
     }
     return x;
 }
 
-bign big_add(bign x,bign y){
-    bign c;
-    int carry=0;
+bign big_short(bign x,bign y){
+    bign z;
     for(int i=0;i<x.len || i<y.len;i++){
-        int temp=x.num[i]+y.num[i]+carry;
-        c.num[c.len++]=temp%10;
-        carry=temp/10;
+        if(x.big_num[i]<y.big_num[i]){
+            x.big_num[i+1]--;
+            x.big_num[i]+=10;
+        }
+        z.big_num[z.len++]=x.big_num[i]-y.big_num[i];
     }
-    if(carry!=0){
-        c.num[c.len]=carry;
-    }
-    return c;
+    return z;
 }
+
 
 int main(){
     char str[2][max];
@@ -43,10 +42,13 @@ int main(){
     }
     bign x=big_save(str[0]);
     bign y=big_save(str[1]);
-    bign ans=big_add(x,y);
+    bign ans=big_short(x,y);
     for(int i=ans.len-1;i>=0;i--){
-        printf("%d",ans.num[i]);
+        printf("%d",ans.big_num[i]);
     }
     system("pause");
     return 0;
 }
+
+//写到这里，发现大整数加法应该也写错了
+//007、008 rework
