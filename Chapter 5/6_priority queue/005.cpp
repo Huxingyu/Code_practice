764592eaca978eb870b1f0c190f63dcf6790ce11
胡行宇 <elgooguknow@gmail.com>
2020-04-11 23:59:21 +0800

    Update 005.cpp

--- Chapter 5/6_priority queue/005.cpp
+++ Chapter 5/6_priority queue/005.cpp
@@ -1,5 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <queue>
 
 using namespace std;
+
+struct fruit{
+    string name;
+    int price;
+    friend bool operator < (fruit x,fruit y){
+        return x.price>y.price;
+    }
+}f1,f2,f3;
+
+int main(){
+    priority_queue<fruit> q;
+    f1.name ="a";
+    f2.name ="b";
+    f3.name ="c";
+    f1.price =1;
+    f2.price =2;
+    f3.price =3;
+    q.push(f1);
+    q.push(f2);
+    q.push(f3);
+    system("pause");
+    return 0;
+}
+
