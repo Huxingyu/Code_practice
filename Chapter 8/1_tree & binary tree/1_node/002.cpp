695591ec120f16696611816d249f930568847428
胡行宇 <elgooguknow@gmail.com>
2020-04-22 23:34:11 +0800

    1

--- Chapter 8/1_tree & binary tree/1_node/002.cpp
+++ Chapter 8/1_tree & binary tree/1_node/002.cpp
@@ -3,3 +3,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct node{
+    int data;
+    node* next;
+};
+
+node* creat(int num[]){
+    node *p,*pre,*head;
+    head=new node;
+    head->next=NULL;
+    pre=head;
+    for(int i=0;i<5;i++){
+        p=new node;
+        p->data=num[i];
+        p->next=NULL;
+        pre->next=p;
+        pre=p;
+    }   
+    return head;
+}
+
+void delet(node* head,int x){
+    node* pre=head;
+    node* temp=head->next;
+    while(temp!=NULL){
+        if(temp->data==x){
+            pre->next=temp->next;
+            delete(temp);
+            temp=pre->next;     //notice
+        }else{
+            pre=temp;
+            temp=temp->next;
+        }
+    }
+}
+
+int main(){
+    int num[5]={1,2,3,4,5};
+    node* ans=creat(num);
+    delet(ans,2);
+    ans=ans->next;
+    while(ans!=NULL){
+        printf("%d ",ans->data);
+        ans=ans->next;
+    }
+    printf("\n");
+    system("pause");
+    return 0;
+}
--- Chapter 8/1_tree & binary tree/1_node/003.cpp
+++ Chapter 8/1_tree & binary tree/1_node/003.cpp
@@ -1,1 +1,49 @@
 //插入
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    node* next;
+};
+
+node* creat(int num[]){
+    node *p,*pre,*head;
+    head=new node;
+    head->next=NULL;
+    pre=head;
+    for(int i=0;i<5;i++){
+        p=new node;
+        p->data=num[i];
+        p->next=NULL;
+        pre->next=p;
+        pre=p;
+    }   
+    return head;
+}
+
+void insert(node* head,int pos,int x){
+    node* temp=head;
+    for(int i=0;i<pos-1;i++){
+        temp=temp->next;
+    }
+    node* mid=new node;
+    mid->data=x;
+    mid->next=temp->next;
+    temp->next=mid;
+}
+
+int main(){
+    int num[5]={1,2,3,4,5};
+    node* ans=creat(num);
+    insert(ans,2,233);
+    ans=ans->next;
+    while(ans!=NULL){
+        printf("%d ",ans->data);
+        ans=ans->next;
+    }
+    printf("\n");
+    system("pause");
+    return 0;
+}
