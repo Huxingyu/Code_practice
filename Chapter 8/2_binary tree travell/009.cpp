4fde6c786c26247a2aff4ae8730281f48d95ea2e
胡行宇 <elgooguknow@gmail.com>
2020-04-24 23:52:54 +0800

    Update 009.cpp

---
+++ Chapter 8/2_binary tree travell/009.cpp
@@ -0,0 +1,40 @@
+//the static of binary tree
+
+#include <stdio.h>
+#include <stdlib.h>
+#define max 10
+
+struct node{
+    int data;
+    int lchild;
+    int rchild;
+}Node[max];
+
+int index=0;
+
+int newNode(int x){
+    Node[index].data=x;
+    Node[index].lchild=-1;
+    Node[index].rchild=-1;
+    return index+1;
+}
+
+void search(int root,int x,int new_data){
+    if(root==-1){
+        return;
+    }
+    if(Node[root].data==x){
+        Node[root].data=new_data;
+    }
+    search(Node[root].lchild,x,new_data);
+    search(Node[root].rchild,x,new_data);
+}
+
+void insert(int root,int x){
+    if(root==-1){
+        root=newNode(x);
+        return;
+    }
+    insert(Node[root].lchild,x);
+    insert(Node[root].rchild,x);
+}
