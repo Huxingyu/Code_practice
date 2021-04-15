5d02a4b3108a73bbd212bb84fa6b641baabc83b1
胡行宇 <elgooguknow@gmail.com>
2021-04-16 00:27:45 +0800

    Update 001.cpp

--- Practice/leetcode/easy/4_etc/001.cpp
+++ Practice/leetcode/easy/4_etc/001.cpp
@@ -2,20 +2,30 @@
 #include <cstdlib>
 #include <vector>
 #include <set>
+#define max 100
 
 using namespace std;
 
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-
+        int n=nums.size();
+        int num[30000]={0},n_num[30000]={0};
+        for(int i=0;i<n;i++){
+            if(nums[i]>=0){
+                num[nums[i]]++;
+            }else{
+                n_num[-nums[i]]++;
+            }   
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>=0 && num[nums[i]]==1){
+                return nums[i];
+            }
+            if(nums[i]<0 && n_num[-nums[i]]==1){
+                return nums[i];
+            }
+        }
+        return 0;
     }
 };
-
-int judge(int num[]){
-    int n=sizeof(num);
-    set<int,bool> st;
-    for(int i=0;i<n;i++){
-        st.insert();
-    }
-}
