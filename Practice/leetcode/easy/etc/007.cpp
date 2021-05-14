d268306186504818ad3120824e1a5cf5c851778c
胡行宇 <elgooguknow@gmail.com>
2021-05-14 23:58:08 +0800

    Update 007.cpp

--- Practice/leetcode/easy/etc/007.cpp
+++ Practice/leetcode/easy/etc/007.cpp
@@ -6,6 +6,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        
+        int ans=cal(0,n);
+        return ans;
+    }
+    int cal(int i,int n){
+        if(i>n){
+            return 0;
+        }
+        if(i==n){
+            return 1;
+        }
+        return cal(i+1,n)+cal(i+2,n);
     }
 };
