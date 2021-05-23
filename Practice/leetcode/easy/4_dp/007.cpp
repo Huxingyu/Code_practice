9ed72d12ba7eab5f4f5c6d03def1848377285d2c
胡行宇 <elgooguknow@gmail.com>
2021-05-23 23:56:40 +0800

    1

--- Practice/leetcode/easy/4_dp/007.cpp
+++ Practice/leetcode/easy/4_dp/007.cpp
@@ -1,1 +1,22 @@
-//5. Longest Palindromic Substring
+//5. Longest Palindromic Substring 
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+class Solution {
+    public:
+        string longestPalindrome(string s) {
+            int n=s.size();
+            for(int i=0;i<n;i++){
+                for(int j=n-1;j>=0;j--){
+                    if(s[j]!=s[i]){
+                        continue;
+                    }
+                    
+                }
+            }
+        }
+};
