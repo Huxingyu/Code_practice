3fce84176c11a6831174ab4acb3dccb5c1df2f96
胡行宇 <elgooguknow@gmail.com>
2021-11-30 23:57:19 +0800

    3

---
+++ test/7 days/C1/009.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    int *p;             //不占用storage space,declaration only
+    int *q=new int;
+    system("pause");
+    return 0;
+}
